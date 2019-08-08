abd278d61ea10dca9662b5daee671940777d9db1
Amir Sasson <amirsas@gmail.com>
2019-08-08 11:45:05 +0300

    remove comments

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -7,13 +7,6 @@ import { HttpClient } from '@angular/common/http';
   providedIn: 'root'
 })
 export class DataService {
-  // this.newsApiKey = '3ef38f994f794ecfbe563db621e56863';
-  // this.greeting = 'אריק לביא 1';
-  // this.location = 'Netanya,il';
-  // this.weatherAppId = '5e6688a21dfb2d2d8e7b4b57b0e88534';
-  // this.magicSeaWeedKey = 'af5ebfb429b64e6c70f8cd3b38a8760a';
-  // this.magicSeaWeedSpot = 4558;
-
   details: BehaviorSubject<IdCard>;
   tenantId: string;
   constructor(private http: HttpClient) {
