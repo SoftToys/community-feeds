a3dbdd1ce10bec5b3d9edff56cfb30de043e1a6c
Amir Sasson <amirsas@gmail.com>
2019-08-29 10:17:01 +0300

    ui responsive

--- src/app/admin/feeds-admin/feeds-list.component.ts
+++ src/app/admin/feeds-admin/feeds-list.component.ts
@@ -4,6 +4,7 @@ import * as moment from 'moment';
 import { HttpClient } from '@angular/common/http';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { EditFeedComponent } from '../edit-feed/edit-feed.component';
+import { IdCard } from 'src/app/details';
 
 @Component({
   selector: 'app-feeds-list',
@@ -19,11 +20,12 @@ export class FeedsListComponent implements OnInit {
   errorMsg: any;
   code: string;
   codeOk: boolean;
+  loading = false;
+  containsChanges = false;
+  card: IdCard;
   constructor(private http: HttpClient, private modalService: NgbModal) { }
 
   ngOnInit() {
-    // this.tenantId = 'ariklavi1';
-    // this.fetchFeeds();
     this.code = localStorage.getItem('admin-code');
     this.codeOk = !!this.code;
   }
@@ -31,32 +33,74 @@ export class FeedsListComponent implements OnInit {
     localStorage.setItem('admin-code', this.code);
     this.codeOk = true;
   }
+
   public fetchFeeds() {
     if (!this.tenantId) {
       return;
     }
+    this.loading = true;
     this.errorMsg = undefined;
     const now = moment();
     const url = `https://communityfeeds.blob.core.windows.net/${this.tenantId}/feeds.json?v=${now.unix()}`;
     this.http.get<Feed[]>(url).subscribe(feeds => {
       this.feeds = feeds;
+      this.loading = false;
     },
       (err: any) => {
+        this.loading = false;
         this.errorMsg = err.statusText || 'unable to fetch resource..';
-      });
+      },
+    );
+  }
+
+  public fetchIdCard() {
+    if (!this.tenantId) {
+      return;
+    }
+    this.loading = true;
+    this.errorMsg = undefined;
+    const now = moment();
+    const url = `https://communityfeeds.blob.core.windows.net/${this.tenantId}/idcard.json?v=${now.unix()}`;
+    this.http.get<IdCard>(url).subscribe(id => {
+      this.card = id;
+      this.loading = false;
+      this.fetchFeeds();
+    },
+      (err: any) => {
+        this.loading = false;
+        this.errorMsg = err.statusText || 'unable to fetch resource..';
+      },
+    );
+  }
+  public cleanCode() {
+    const clean = confirm('Are you sure you want to logout?');
+    if (clean) {
+      this.code = undefined;
+      this.codeOk = false;
+      this.card = undefined;
+      this.feeds = undefined;
+      localStorage.removeItem('admin-code');
+    }
+  }
+
+  public cleanTenant() {
+    this.card = undefined;
+    this.feeds = undefined;
   }
 
   public setActive(feed: Feed, active: boolean) {
     feed.isActive = active;
+    this.containsChanges = true;
   }
-
 
   /**
    * open
    *   const modalRef = this.modalService.open(NgbdModalContent);
    */
   public open(feed?: Feed) {
+
     const modalRef = this.modalService.open(EditFeedComponent, { size: 'lg' });
+    this.containsChanges = true;
     modalRef.componentInstance.feed = feed || { isActive: true };
     modalRef.result.then((result) => {
       if (feed) { // new
@@ -71,11 +115,15 @@ export class FeedsListComponent implements OnInit {
   public publish() {
     const publish = confirm('Are you sure you want to publish changes?');
     if (publish) {
+      this.loading = true;
       this.http.post(`https://feeds-admin.azurewebsites.net/api/HttpTrigger1?tenantId=${this.tenantId}&code=${this.code}`,
-        this.feeds).subscribe((i) => {
-
-        },
+        this.feeds).subscribe(
+          (i) => {
+            this.loading = false;
+            this.containsChanges = false;
+          },
           (err) => {
+            this.loading = false;
             if (err.statusText !== 'OK') {
               this.errorMsg = err.statusText || 'unable to save resources.. try again';
             }
@@ -93,7 +141,6 @@ export class FeedsListComponent implements OnInit {
   }
 
   public edit(content, feed: Feed) {
-    this.currentlyEditing = feed;
     this.modalService.open(content, { ariaLabelledBy: 'modal-basic-title' }).result.then((result) => {
       this.closeResult = `Closed with: ${result}`;
     }, (reason) => {
